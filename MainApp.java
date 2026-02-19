ee7173800e1e189713c9dcda8c66f4822de1615d
KariukiAddi <kelvinngigekariuki@gmail.com>
2026-02-19 19:21:41 +0300

    Added number of students to 4

--- MainApp.java
+++ MainApp.java
@@ -6,13 +6,14 @@ public class MainApp {
         // Create Course objects
         Course course1 = new Course("Object Oriented Software Design Methods", "OOSDM101");
         Course course2 = new Course("Data Structures and Algorithms", "DSA102");
-
+        Course course3 = new Course("Object Oriented Software Design Methods", "OOSDM101");
+        Course course4 = new Course("Data Structures and Algorithms", "DSA102");
         // Create Student objects
         Student student1 = new Student(1, "Kelvin Kariuki", 95, course1);
         Student student2 = new Student(2, "Grace Adhiambo", 90, course2);
-        Student student3 = new Student(3, "Jane wambua", 85, course1);
-        Student student4 = new Student(4, "Mutisya Simiyu", 80, course2);
-        
+        Student student3 = new Student(3, "Jane wambua", 85, course3);
+        Student student4 = new Student(4, "Mutisya Simiyu", 80, course4);
+
         // Display student details
         student1.displayDetails();
         student2.displayDetails();
